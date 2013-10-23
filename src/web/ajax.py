12cffa7f1be62ea61429dc3a37a78e9958e6a8a4
renzon <renzon@gmail.com>
2013-10-23 16:15:58 -0200

    2

--- src/web/ajax.py
+++ src/web/ajax.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import json
+import logging
 from user.model import User
 
 
 def save_user(_resp, name):
     user = User(name=name)
     user.put()
+    logging.info("Svaing %s" % user)
     js = json.dumps(user.to_dict())
     _resp.write(js)
